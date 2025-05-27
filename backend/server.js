d7c569aa125f4f308a733ec39e7877a0eec67a58
Sandip Suthar <sandipsuthar086@gmail.com>
2025-05-27 20:28:07 +0530

    remove cors function

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ const multer = require('multer');
 const multerGridFSStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 
-app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:5174'], // Allow requests from your frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // If using cookies
-}));
+
 
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // Increase URL-encoded payload size limit
 app.use(express.json({ limit: '100mb' })); // Increase JSON payload size limit
